583cac501855d195769ec9c0ac05c1242cbac55d
Danish Khan <dkhan1010@gmail.com>
2025-08-29 21:30:12 -0700

    Update tokens.cpp

--- codes/tokens.cpp
+++ codes/tokens.cpp
@@ -1,22 +1,3 @@
-# Tokens example code
-<div class="mt-10"></div>
-
-<div class="mt-10"></div>
-<div class="flex items-center justify-between w-full">
-  <span class="px-2 py-1 text-xs font-medium !text-white bg-blue-800 rounded">
-    tokens.cpp | Example code shows tokens in C++
-  </span>
-  <a href="https://github.com/d-khan/cpp/blob/main/codes/printing_add.cpp"
-     target="_blank"
-     rel="noopener noreferrer"
-     class="inline-block px-2 py-1 rounded bg-blue-600 !text-white text-xs font-medium no-underline hover:bg-blue-800 transition-colors duration-200 whitespace-nowrap outline-none focus:ring-0">
-    Copy code
-  </a>
-</div>
-
-````md magic-move {lines: true}
-```cpp {*}
-
 #include <iostream>   // Preprocessor directive (not a token, but expands before tokens)
 
 using namespace std;  // Keywords: using, namespace
@@ -34,5 +15,3 @@ int main() {          // Keyword: int, Identifier: main, Punctuators: ()
 
     return 0;         // Keyword: return | Literal: 0
 }
-```
-````
